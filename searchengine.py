9665a54379ea85543ece8582fb5bf69752cd96a5
Austin Chiang <achiang5@uci.edu>
2018-11-21 11:28:23 -0800

    Added beautifulSoup functionality

--- searchengine.py
+++ searchengine.py
@@ -1,7 +1,20 @@
 from bs4 import BeautifulSoup
 
+htmlFile = open("6","r");
 
+soup = BeautifulSoup(htmlFile,'html.parser')
 
-soup = BeautifulSoup(DOCNAME ,'html.parser')
+"""
+with open("6") as fp:
+    soup = BeautifulSoup(fp)
 
-print(soup.prettify())
+soup = BeautifulSoup("<html>data</html")
+"""
+
+for script in soup.find_all('script'):
+    script.extract()
+
+for style in soup.find_all('style'):
+    style.extract()
+    
+print(soup.get_text())
