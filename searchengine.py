484d6e0aaa3d5db38f171accb2d238c48808b674
Bianca Tang <tang.bianca13@gmail.com>
2018-11-21 13:45:31 -0800

    PartA repurposed to parse through given webpages

--- searchengine.py
+++ searchengine.py
@@ -1,7 +1,23 @@
 from bs4 import BeautifulSoup
+import io
 
 
+htmlFile = open("6","r");
 
-soup = BeautifulSoup(DOCNAME ,'html.parser')
+with io.open("6", "r", encoding="utf-8", errors='ignore') as fp:
+    soup = BeautifulSoup(fp)
 
-print(soup.prettify())
+for script in soup.find_all('script'):
+    script.extract()
+
+for style in soup.find_all('style'):
+    style.extract()
+
+for href in soup.find_all('href'):
+    href.extract()
+
+print(soup.get_text().encode("utf-8"))
+for string in soup.stripped_strings:
+    print(string.encode("utf-8"))
+
+fp.close()
