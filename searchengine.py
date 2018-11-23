c4df0b28e6f03e37434d9fad273ce2e853c9b420
Austin Chiang <achiang5@uci.edu>
2018-11-23 12:15:49 -0800

    Added search engine report and search query functionality

--- searchengine.py
+++ searchengine.py
@@ -26,50 +26,50 @@ if __name__ == "__main__":
                 filepath = subdir + os.sep + file
                 docName = folderName + '.' + file
                 
-                if("WEBPAGES_RAW\\0" in filepath): #== "C:\\Users\\Biancat\\Documents\\CS121-InfoRetrieval\\121-Project-3\\WEBPAGES_RAW\\0\\6"):
-                    print(docName)
-                    
-                    with io.open(filepath, "r", encoding="utf8", errors='ignore') as fp:
-                        soup = BeautifulSoup(fp)
+                #if("WEBPAGES_RAW\\0" in filepath): #== "C:\\Users\\Biancat\\Documents\\CS121-InfoRetrieval\\121-Project-3\\WEBPAGES_RAW\\0\\6"):
+                #print(docName)
+                
+                with io.open(filepath, "r", encoding="utf8", errors='ignore') as fp:
+                    soup = BeautifulSoup(fp)
 
-                    for script in soup.find_all('script'):
-                        script.extract()
+                for script in soup.find_all('script'):
+                    script.extract()
 
-                    for style in soup.find_all('style'):
-                        style.extract()
+                for style in soup.find_all('style'):
+                    style.extract()
 
-                    for href in soup.find_all('href'):
-                        href.extract()
+                for href in soup.find_all('href'):
+                    href.extract()
 
-                    #sortedFreq has the log-freq weighted term frequency: [item(token:weighted term freq)]
-                    #w = 1 + log10(tf)
-                    sortedFreq = PartA.outputFrequencies(soup.stripped_strings) 
-                    #calculate the tf-idf of each term, and stick it in the filesFreq
-                    for item in sortedFreq:
-                        #fill in document frequency dict
-                        if item[0] not in docFreq:
-                            docFreq[item[0]] = 1
-                        else:
-                            docFreq[item[0]] += 1
-                        if item[0] not in invertedIndex:
-                            invertedIndex[item[0]] = dict({docName:item[1]*(log10(numFiles/docFreq[item[0]]))})
-                        else: #token may be in there, but the doc is new, so add to the doc/tf-idf dict
-                            #idf = log10(N/df)
-                            #tf-idf = tf x idf
-                            invertedIndex[item[0]][docName] = item[1]*(log10(numFiles/docFreq[item[0]]))
+                #sortedFreq has the log-freq weighted term frequency: [item(token:weighted term freq)]
+                #w = 1 + log10(tf)
+                sortedFreq = PartA.outputFrequencies(soup.stripped_strings) 
+                #calculate the tf-idf of each term, and stick it in the filesFreq
+                for item in sortedFreq:
+                    #fill in document frequency dict
+                    if item[0] not in docFreq:
+                        docFreq[item[0]] = 1
+                    else:
+                        docFreq[item[0]] += 1
+                    if item[0] not in invertedIndex:
+                        invertedIndex[item[0]] = dict({docName:item[1]*(log10(numFiles/docFreq[item[0]]))})
+                    else: #token may be in there, but the doc is new, so add to the doc/tf-idf dict
+                        #idf = log10(N/df)
+                        #tf-idf = tf x idf
+                        invertedIndex[item[0]][docName] = item[1]*(log10(numFiles/docFreq[item[0]]))
 
-                    '''
-                    for item in sortedFreq:
-                        if item[0] not in fileFreq:
-                            fileFreq[item[0]] = item[1]
-                        else:
-                            fileFreq[item[0]] += item[1]
-                    '''     
-                    '''
-                    for string in soup.stripped_strings:
-                        print(string.encode("utf-8"))
-                    '''        
-                    fp.close()
+                '''
+                for item in sortedFreq:
+                    if item[0] not in fileFreq:
+                        fileFreq[item[0]] = item[1]
+                    else:
+                        fileFreq[item[0]] += item[1]
+                '''     
+                '''
+                for string in soup.stripped_strings:
+                    print(string.encode("utf-8"))
+                '''        
+                fp.close()
 
     except IOError:
             print("File not found, or path is incorrect")
@@ -97,5 +97,6 @@ if __name__ == "__main__":
             indexFile.write(vk + " " + str(vv) + '\n')
     indexFile.close()
     print("Created index!")
+
     
     
---
+++ searchquery.py
@@ -0,0 +1,48 @@
+import string
+import sys
+import re
+import io
+import heapq
+
+searchQuery = ""
+
+totalWords = 0
+docFreq = dict() #{token:numberOfFilesTokenIsFoundIn}
+invertedIndex = dict() #token:dict{docID:tf-idf} A DICT WITH DICT VALUES
+
+
+#opens file and creates dictionary from inverted index file
+invertedIndexFile = io.open("invIndex.txt","r", encoding = 'utf8', errors = 'ignore')
+singleLine = invertedIndexFile.readline()
+       
+while singleLine:
+    allTokensInLine = re.split(' ',singleLine.encode('utf8'))
+    count = 0
+    for token in allTokensInLine:
+        if token[len(token)-2] == ":":
+            indexToken = token[:(len(token)-2)]
+            invertedIndex[indexToken[:(len(token)-2)]] = dict()
+            totalWords += 1
+            break
+                          
+        if count == 0:
+            docName = token
+
+        if count == 1:
+            invertedIndex[indexToken][docName] = token.strip()
+
+        count += 1
+                          
+    singleLine = invertedIndexFile.readline()
+ 
+while searchQuery != "quit":
+    print("Enter Search Query:")
+    searchQuery = raw_input().lower().strip()
+    for key in invertedIndex[searchQuery]:
+        print(key)
+        #print(+ " " + invertedIndex[searchQuery][key])
+
+
+print(len(invertedIndex))
+print(totalWords)
+   
