926bdc6ca2e039d528aee36df1850aefbf00b7dc
Bianca Tang <tang.bianca13@gmail.com>
2018-12-04 22:21:52 -0800

    Fixed title parsing bug

--- searchengine.py
+++ searchengine.py
@@ -4,6 +4,7 @@ import os
 import PartA
 from math import log10
 import string
+from collections import defaultdict
 
 
 if __name__ == "__main__":
@@ -15,10 +16,10 @@ if __name__ == "__main__":
     Doc: <folder>/<file> -> Weight: <tf-idf>
     '''
     
-    docFreq = dict() #{token:numberOfFilesTokenIsFoundIn}
-    invertedIndex = dict() #token:dict{docID:tf-idf} A DICT WITH DICT VALUES
+    docFreq = defaultdict() #{token:numberOfFilesTokenIsFoundIn}
+    invertedIndex = defaultdict() #token:dict{docID:tf-idf} A DICT WITH DICT VALUES
     numFiles = 0;
-    snippetsDict = dict() #{docID:title)
+    snippetsDict = defaultdict() #{docID:title)
     try:
         for subdir, dirs, files in os.walk(os.getcwd()+'\WEBPAGES_RAW'):
             folderName = subdir[len(os.getcwd())+14:]
@@ -42,7 +43,7 @@ if __name__ == "__main__":
                 #grab the title
                 if soup.title != None:
                     title = ''
-                    titleWords = str(soup.title).strip("<title>").strip("</title>").strip().split()
+                    titleWords = str(soup.title).strip("<title>").strip("</title>").replace('<',' <').replace('>', '> ').strip().split()
                     for word in titleWords:
                         if word[0] != '<':
                             for letter in word:
@@ -51,6 +52,7 @@ if __name__ == "__main__":
                             title += " "
                     if (title.strip() != "" and title.strip() != " "):
                         snippetsDict[docName] = title.strip()
+                        print(title.strip())
                 
                 #sortedFreq has the log-freq weighted term frequency for EACH term in one file: [item(token:weighted term freq)]
                 #w = 1 + log10(tf)
@@ -65,10 +67,12 @@ if __name__ == "__main__":
                         invertedIndex[item[0]] = dict({docName:item[1]}) #for now, store tf. Then, after we know how total docs, calc and mult. by IDF
                     else: #token may be in there, but the doc is new       
                         invertedIndex[item[0]][docName] = item[1]
+                
                 fp.close()
 #-----------------------------------------------------------------------------------------
 #GENERATE THE TF-IDF FOR EACH DOCUMENT IN EACH TOKEN DICT
 #note: the tf is already stored for each document. Just multiply it by idf
+        
         print("Going through and calculating tf-idfs")
         for token, docDict in invertedIndex.items():
             #idf = log10(N/df)
@@ -76,19 +80,19 @@ if __name__ == "__main__":
             for docID, tf in docDict.items():
                 docDict[docID] = tf * log10(numFiles/docFreq[token])
         print("Calculated tf-idfs")
-
+        
     except IOError:
             print("File not found, or path is incorrect")
     except Exception as ex:
         print(ex.message)
-
+        
 #----------------------------------------------------------------------------------------
 #FILE WRITING: INVERTED INDEX AND SNIPPETS (TITLES IF THEY EXIST) DICT
-
+    
     #write the index to a file in the format:
     '''
-    <token>:
-    <folder>/<file> <tf-idf weight>
+    #<token>:
+    #<folder>/<file> <tf-idf weight>
     '''
     indexFile = open('invIndex.txt', 'w+')
     for k,v in invertedIndex.items():
@@ -98,10 +102,10 @@ if __name__ == "__main__":
     indexFile.close()
     print("Created index!")
 
-
+    
     #write snippetsDict to a file in the format:
     '''
-    <folder>/<file> title firstTwenty
+    <folder>/<file> title
     '''
     snippetsFile = open('snippets.txt', 'w+')
     for k,v in snippetsDict.items(): #k is docName, v is title
